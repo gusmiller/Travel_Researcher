65f03478cc4bdf6b6ff6d7cbf069674d5ee75302
Sonja <sonjaljatkovic@gmail.com>
2023-09-19 18:24:04 -0400

    added function that returns values from user inputs

--- assets/js/flights.js
+++ assets/js/flights.js
@@ -1,4 +1,7 @@
-var flightsApiKey = "bc62d3c507d8b552c56639324618f98b"
+var flightsApiKey = "bc62d3c507d8b552c56639324618f98b";
+var departureCityInputEl = document.querySelector("#departure-city-input");
+var destinationCityInputEl = document.querySelector("#destination-city-input");
+var departureDateInputEl = document.querySelector("#departure-date-input");
 
 
 
@@ -12,3 +15,18 @@ var storeSearchedFlights = function() {
 var startingWithCapital = function(str) {
     return str.replace(/\b\w/g, x => x.toUpperCase());
 }
+
+var searchParameters = function() {
+    var departureCityInput = departureCityInputEl.value.trim();
+    var departureCityCapital = startingWithCapital(departureCityInput);
+    var destinationCityInput = destinationCityInputEl.value.trim();
+    var destinationCityCapital = startingWithCapital(destinationCityInput);
+    var departureDateInput = departureDateInputEl.value.trim();
+    return [departureCityCapital, destinationCityCapital, departureDateInput]
+}
+
+var searchFlights = function(event) {
+    event.preventDefault();
+
+}
+
