edcdee907d85d3b49d9d322baf946929520e6173
Sonja <sonjaljatkovic@gmail.com>
2023-09-19 19:42:31 -0400

    added functions to build object for local storage and added event listener

--- assets/js/flights.js
+++ assets/js/flights.js
@@ -2,6 +2,7 @@ var flightsApiKey = "bc62d3c507d8b552c56639324618f98b";
 var departureCityInputEl = document.querySelector("#departure-city-input");
 var destinationCityInputEl = document.querySelector("#destination-city-input");
 var departureDateInputEl = document.querySelector("#departure-date-input");
+var searchFlightButtonEl = document.querySelector("#search-flight-button");
 
 
 
@@ -9,7 +10,7 @@ var departureDateInputEl = document.querySelector("#departure-date-input");
 var searchedFlights = {};
 
 var storeSearchedFlights = function() {
-    localStorage.setItem("storeSearchedFlights", JSON.stringify(searchedFlights));
+    localStorage.setItem("Store-searched-flights", JSON.stringify(searchedFlights));
 }
 
 var startingWithCapital = function(str) {
@@ -27,6 +28,20 @@ var searchParameters = function() {
 
 var searchFlights = function(event) {
     event.preventDefault();
-
+    let [departureCityCapital, destinationCityCapital, departureDateInput] = searchParameters();
+    if (!departureCityCapital || !destinationCityCapital || !departureDateInput) {
+        return; 
+        // pop up message will show
+    }
+    saveSearchedFlights(departureCityCapital, destinationCityCapital, departureDateInput)
 }
 
+var saveSearchedFlights = function(departureCityCapital, destinationCityCapital, departureDateInput) {
+    searchedFlights["departure-city"] = departureCityCapital;
+    searchedFlights["destination-city"] = destinationCityCapital;
+    searchedFlights["departure-date"] = departureDateInput;
+    storeSearchedFlights();
+}
+
+
+searchFlightButtonEl.addEventListener("click", searchFlights);
