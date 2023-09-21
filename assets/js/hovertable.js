d0d95c743a334580b4093e5d64a5f6c23cae4d64
Gus Miller <gustavo.miller@miller-hs.com>
2023-09-20 21:55:10 -0400

    feature-hover - add comments and final changes

--- assets/js/hovertable.js
+++ assets/js/hovertable.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
 
-      // Use combination of JQuery and Javascrips
-      const popup = document.getElementById('popup');
+      // Jquery selector to retrieve popup
+      const popup = $("#popup");
 
       /**
        * This function will trigger the select flight. The information in the <tr> will be 
        * added into the Local Storage
        */
-      function selectFlight() {
-            var row = $("#" + this.parentElement.parentElement.id); // Retrieve the row element
+      function selectRow() {
+
+            // Jquery selector using ID to retrieve the row element
+            const row = $("#" + this.parentElement.parentElement.id); 
 
             // Retrieve items using JQuery traversing; moving though the html
             // https://www.w3schools.com/jquery/jquery_traversing.asp
@@ -27,30 +29,39 @@ $(document).ready(function () {
        */
       function removeButton() {
 
-            // Remove button element
+            // Remove all buttons from area - there is only 1 button
             // https://developer.mozilla.org/en-US/docs/Web/API/Element/remove
-            $("#selectFlight").remove(); // Build the id in string
+            $("#selectFlight").remove(); 
       }
 
       /**
-       * This function will add a new button to the section where the user has 
-       * hover their mouse
+       * This function will add a new button to the row where user has 
+       * hover (mouseover) their mouse. It assigns the an ID to the button, and adds the rest of the 
+       * attributes. It appends the new button on the last column and assigns a event.
        */
       function revealButton() {
-            var buttonArea = $("#" + this.id); // Build the id in string
-            var newElement = $("<button></button>") // Create new button
+            var buttonArea = $("#" + this.id); // Create variable using JQuery selctor
+
+            // Jquer to create new button
+            var newElement = $("<button></button>") 
             newElement.attr("id", "selectFlight"); // Assign an ID
             newElement.attr("type", "button"); // Assign an ID
             newElement.text("Select Flight"); // Add text to button
+
+            // Add multiple classes 
+            // https://www.w3schools.com/jquery/html_addclass.asp
             newElement.addClass("px-5 py-1 border-blue-500 border text-sm text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none"); // Add CSS classes
 
             // The button is added to section dynamically. Validate whether the button is are already existing
             if (buttonArea.children().eq(buttonArea.children().length - 1).children().length == 0) {
+                  
+                  // Appends button using the append class
+                  // https://developer.mozilla.org/en-US/docs/Web/API/Element/append
                   buttonArea.children().eq(buttonArea.children().length - 1).append(newElement); // Append new button      
 
                   // Add event to selected flight button
                   // https://developer.mozilla.org/en-US/docs/Web/API/Element/click_event
-                  $("#selectFlight").on("click", selectFlight);
+                  $("#selectFlight").on("click", selectRow);
             }
       }
 
@@ -63,8 +74,8 @@ $(document).ready(function () {
             // Assign an event to ALL buttons added dynamically into the DOM. These buttons originally
             // will NOT be there. To reset the array use the inspect to delete the LocalStorage
             // https://developer.mozilla.org/en-US/docs/Web/API/Element/click_event
-            $(".tablerow").on("mouseover", revealButton);
-            $(".tablerow").on("mouseleave", removeButton);
+            $(".tablerow").on("mouseover", revealButton);               // SONJA: the row NEEDS to have a dummy class - tablerow
+            $(".tablerow").on("mouseleave", removeButton);              // SONJA: the row NEEDS to have a dummy class - tablerow
             $("#close-popup").on("click", function () {
                   popup.classList.add('hidden');
             });
