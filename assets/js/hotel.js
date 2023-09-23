fe2db53315402eb243c04cbcd5f5e83a4e569087
leviathan <jake66martin@hotmail.com>
2023-09-23 14:34:07 -0400

    fixed date input format for fetch

--- assets/js/hotel.js
+++ assets/js/hotel.js
@@ -4,9 +4,6 @@ var checkOut = document.querySelector("#checkout-date-input")
 var city = document.querySelector("#city")
 var searchButton = document.querySelector("#searchbtn")
 
-var checkInValue = checkIn.value
-var checkOutValue = checkOut.value
-
 
 searchButton.addEventListener("click", (event) => {
 
@@ -32,12 +29,39 @@ var fetchHotels = function (city) {
         return response.json();
     })
     .then(function(data){
+        
+        var cityId = data[0].dest_id
+
+        var searchHotel = "https://booking-com.p.rapidapi.com/v1/hotels/search?checkin_date=" + checkIn.value + "&dest_type=city&units=metric&checkout_date=" + checkOut.value + "&adults_number=2&order_by=popularity&dest_id=" + cityId + "&filter_by_currency=AED&locale=en-gb&room_number=1&children_number=2&children_ages=5%2C0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&page_number=0&include_adjacency=true"
+
+        fetch(searchHotel, {
+            method: "GET",
+            headers: {
+                'X-RapidAPI-Key': '297373a073msh7b0598ca6341179p15ab78jsn93aac3e1c65e',
+                'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
+            }
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data){
+            
+            
+            console.log(data);
+    
+    
+    
+            
+        })
         console.log(data);
+
+
 
         
     })
 }
 
+console.log(checkIn.value)
 
 
 
