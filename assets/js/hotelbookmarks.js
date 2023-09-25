6ba2be68e795684fad6527228d54f2a7f365000a
leviathan <jake66martin@hotmail.com>
2023-09-25 18:59:31 -0400

    changed bookmarks table and added style to buttons

--- assets/js/hotelbookmarks.js
+++ assets/js/hotelbookmarks.js
@@ -14,25 +14,23 @@ function createRows (getHotels) {
 
 var hotelRowEl = document.createElement("tr");
     hotelRowEl.setAttribute("class", "hotel-information-row");
-    var cityName1 = document.createElement("td")
-    cityName1.textContent = getHotels.cityName
-    hotelRowEl.appendChild(cityName1)
+   
     var hotelName1 = document.createElement("td")
     hotelName1.textContent = getHotels.hotelName
     hotelRowEl.appendChild(hotelName1)
     var hotelAddress1 = document.createElement("td")
     hotelAddress1.textContent = getHotels.hotelAddress
     hotelRowEl.appendChild(hotelAddress1)
-    var checkIn1 = document.createElement("td")
-    checkIn1.textContent = getHotels.checkinDate
-    hotelRowEl.appendChild(checkIn1)
-    var checkOut1 = document.createElement("td")
-    checkOut1.textContent = getHotels.checkoutDate
-    hotelRowEl.appendChild(checkOut1)
+    
+   
     var totalPrice1 = document.createElement("td")
     totalPrice1.textContent = getHotels.totalPrice
     hotelRowEl.appendChild(totalPrice1)
     renderAllRows(getHotels)
+    var bookmarkHotels = document.createElement("button")
+    bookmarkHotels.textContent = "Book Hotel"
+    bookmarkHotels.setAttribute("class", "transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ...")
+    hotelRowEl.appendChild(bookmarkHotels)
     return hotelRowEl
 }
 
