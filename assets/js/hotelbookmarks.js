593e10bf743e62f7218b35b0c7f69c70b83dab19
Sonja <sonjaljatkovic@gmail.com>
2023-09-26 20:23:12 -0400

    put everything together

---
+++ assets/js/hotelbookmarks.js
@@ -0,0 +1,61 @@
+// 
+
+var hotelTable = document.querySelector(".bookmark-table")
+const openflightModal = document.getElementById('openModal');
+const closeflightModal = document.getElementById('closeModal');
+const closeModal = document.getElementById('close-popup');
+const warning = document.getElementById('warningModal');
+ var loadData = window.addEventListener("load", () => {
+    var getHotels = JSON.parse(localStorage.getItem("storedHotels"));
+    console.log(getHotels)
+    createRows(getHotels)
+})
+var transformData = function(){
+}
+transformData()
+var displayBookedHotelPopup = function(event) {
+    console.log("click")
+    var getHotels = event.target.dataset;
+    console.log(getHotels);
+    $("#city").val(getHotels.city);
+    $("#hotel").val(getHotels.hotelName);
+    $("#address").val(getHotels.hotelAddress);
+    $("#price").val(getHotels.totalPrice);
+    $("#popup").removeClass("hidden");
+    closeModal.addEventListener("click", () =>
+    $("#popup").addClass("hidden")
+    )
+    return getHotels
+}
+function createRows (getHotels) {
+var hotelRowEl = document.createElement("tr");
+    hotelRowEl.setAttribute("class", "hotel-information-row flex flex-col mb-4 sm:table-row");
+    var hotelName1 = document.createElement("td")
+    hotelName1.textContent = getHotels.hotelName
+    hotelName1.setAttribute("class", "p-2 border border-slate-300 ...")
+    hotelRowEl.appendChild(hotelName1)
+    var hotelAddress1 = document.createElement("td")
+    hotelAddress1.textContent = getHotels.hotelAddress
+    hotelAddress1.setAttribute("class", "p-2 border border-slate-300 ...")
+    hotelRowEl.appendChild(hotelAddress1)
+    var totalPrice1 = document.createElement("td")
+    totalPrice1.textContent = getHotels.totalPrice
+    totalPrice1.setAttribute("class", "p-2 border border-slate-300 ...")
+    hotelRowEl.appendChild(totalPrice1)
+    renderAllRows(getHotels)
+    var bookmarkHotels = document.createElement("button")
+    bookmarkHotels.textContent = "Book Hotel"
+    bookmarkHotels.setAttribute("class", "transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ...")
+    bookmarkHotels.setAttribute("data-hotel-name", getHotels.hotelName);
+    bookmarkHotels.setAttribute("data-hotel-address", getHotels.hotelAddress);
+    bookmarkHotels.setAttribute("data-total-price", getHotels.totalPrice);
+    bookmarkHotels.setAttribute("data-city", getHotels.city);
+    hotelRowEl.appendChild(bookmarkHotels)
+    bookmarkHotels.addEventListener("click", displayBookedHotelPopup)
+    return hotelRowEl
+}
+function renderAllRows(getHotels) {
+    for (var i = 0; i < getHotels.length; i++){
+        hotelTable.appendChild(createRows(getHotels[i]));
+    }
+}
